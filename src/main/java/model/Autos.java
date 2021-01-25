cd5a0dc6441bde58546ac7b120121c4e4c439bd4
Gradio2000 <laskin999@gmail.com>
2021-01-25 10:29:12 +0500

    Initial commit

---
+++ src/main/java/model/Autos.java
@@ -0,0 +1,78 @@
+package model;
+
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Autos {
+    private int autoId;
+    private String model;
+    private int speed;
+    private String color;
+
+    @Id
+    @Column(name = "auto_id", nullable = false)
+    public int getAutoId() {
+        return autoId;
+    }
+
+    public void setAutoId(int autoId) {
+        this.autoId = autoId;
+    }
+
+    @Basic
+    @Column(name = "model", nullable = true, length = 30)
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    @Basic
+    @Column(name = "speed", nullable = false)
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    @Basic
+    @Column(name = "color", nullable = false, length = 10)
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Autos autos = (Autos) o;
+
+        if (autoId != autos.autoId) return false;
+        if (speed != autos.speed) return false;
+        if (model != null ? !model.equals(autos.model) : autos.model != null) return false;
+        if (color != null ? !color.equals(autos.color) : autos.color != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = autoId;
+        result = 31 * result + (model != null ? model.hashCode() : 0);
+        result = 31 * result + speed;
+        result = 31 * result + (color != null ? color.hashCode() : 0);
+        return result;
+    }
+}
---
+++ src/main/java/model/Persons.java
@@ -0,0 +1,79 @@
+package model;
+
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Persons {
+    private int personId;
+    private String name;
+    private int age;
+    private String email;
+
+    @Id
+    @Basic
+    @Column(name = "person_id", nullable = false)
+    public int getPersonId() {
+        return personId;
+    }
+
+    public void setPersonId(int personId) {
+        this.personId = personId;
+    }
+
+    @Basic
+    @Column(name = "name", nullable = false, length = -1)
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Basic
+    @Column(name = "age", nullable = false)
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Basic
+    @Column(name = "email", nullable = true, length = -1)
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Persons persons = (Persons) o;
+
+        if (personId != persons.personId) return false;
+        if (age != persons.age) return false;
+        if (name != null ? !name.equals(persons.name) : persons.name != null) return false;
+        if (email != null ? !email.equals(persons.email) : persons.email != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = personId;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        result = 31 * result + age;
+        result = 31 * result + (email != null ? email.hashCode() : 0);
+        return result;
+    }
+}
